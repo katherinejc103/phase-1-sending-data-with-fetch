0ad150015b9d9ac28302afad48ecd5156fdbfebb
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-03-01 20:51:03 -0500

    commit tree 6d328069227c54bc6b53973289c81fb64639af53

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(userName , userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: userName, email: userEmail})
+    })
+    .then(response => response.json())
+    .then(function (object) {
+        document.body.innerHTML = object.id})
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
+// submitData('http://localhost:3000/users', )
